2b7aef2500aed5cd6bedded72cdcc479f8ebfdbf
shayan <shayan.yousefi100@yahoo.com>
2023-04-22 11:27:19 +0330

    adds support for sqlite db

--- src/Traits/ReservesData/GetReserves.php
+++ src/Traits/ReservesData/GetReserves.php
@@ -3,13 +3,21 @@
 namespace ShayanYS\LaraReserve\Traits\ReservesData;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Support\Facades\DB;
 
 trait GetReserves
 {
 
     public function activeReserves(): MorphMany{
-        return $this->reserves()->where([[DB::raw('STR_TO_DATE(concat(`reserved_date`," ",`reserved_time`),"%Y-%m-%d %H:%i:%s")'), '>=', now()]])->with(['customer','reservable']);
+
+        if(DB::connection() instanceof SQLiteConnection)
+        {
+            return $this->reserves()->where([[DB::raw('strftime("%Y-%m-%d %H:%M:%S",strftime("%Y-%m-%d",`reserved_date`) || " " || `reserved_time`)'), '>=', now()]])->with(['customer','reservable']);
+        }
+        return $this->reserves()->where([[DB::raw('STR_TO_DATE(concat(`reserved_date` , " " , `reserved_time`),"%Y-%m-%d %H:%i:%s")'), '>=', now()]])->with(['customer','reservable']);
+
     }
     public function allReserves():  MorphMany{
         return $this->reserves()->with(['customer','reservable']);
