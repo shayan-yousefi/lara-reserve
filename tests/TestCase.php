125ba12e6f24b391998f439320ba116c5d4e3475
shayan <shayan.yousefi100@yahoo.com>
2023-04-22 11:28:42 +0330

    migrations added to run for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,7 +22,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getEnvironmentSetUp($app)
     {
 
-        $migration = include dirname(__DIR__) . '/database/migrations/create_reserves_table.php';
-        $migration->up();
+        $migrationReserve = include dirname(__DIR__) . '/database/migrations/create_reserves_table.php';
+        $migrationReservable = include __DIR__ . '/TestsMigrations/2023_04_22_103322_create__reservable_test_models_table.php';
+        $migrationCustomer = include __DIR__ . '/TestsMigrations/2023_04_22_103311_create__customer_test_models_table.php';
+        $migrationReserve->up();
+        $migrationReservable->up();
+        $migrationCustomer->up();
     }
 }
