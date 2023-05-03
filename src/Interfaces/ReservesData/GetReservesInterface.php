5719feafc081a3a83d05264c0588c4a77af371b2
Shayan Yousefi <shayan.yousefi100@yahoo.com>
2023-05-03 19:42:53 +0330

    two new method added: startedReserves - endedReserves

--- src/Interfaces/ReservesData/GetReservesInterface.php
+++ src/Interfaces/ReservesData/GetReservesInterface.php
@@ -10,4 +10,6 @@ interface GetReservesInterface
 {
     public function activeReserves(): MorphMany;
     public function allReserves():  MorphMany;
+    public function startedReserves(): MorphMany;
+    public function endedReserves(): MorphMany;
 }
