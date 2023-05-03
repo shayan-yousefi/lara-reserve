a065cd7f868905b48e0a1a56c8b3d361eaae63fd
Shayan Yousefi <shayan.yousefi100@yahoo.com>
2023-05-03 17:03:58 +0330

    end reserve date and time added to reserve functions

--- database/migrations/create_reserves_table.php
+++ database/migrations/create_reserves_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->json('metadata')->nullable();
             $table->date('reserved_date');
             $table->time('reserved_time')->default('00:00:00');
+            $table->date('until_reserved_date');
+            $table->time('until_reserved_time')->default('00:00:00');
             $table->timestamps();
         });
     }
--- src/Interfaces/Reserves/CustomerReserveInterface.php
+++ src/Interfaces/Reserves/CustomerReserveInterface.php
@@ -8,5 +8,5 @@ use ShayanYS\LaraReserve\Interfaces\ReservableInterface;
 use ShayanYS\LaraReserve\Models\Reserve;
 
 interface  CustomerReserveInterface{
-    public function reserve(ReservableInterface $reservable, DatetimeInterface|Carbon|DateTime $reserveDate, string $reserveTime = '00:00:00', ?array $metadata = null): Reserve | bool;
+    public function reserve(ReservableInterface $reservable, DatetimeInterface|Carbon|DateTime $reserveDate, string $reserveTime = '00:00:00',null|DatetimeInterface|Carbon|DateTime $endReserveDate = null, ?string $endReserveTime = null, ?array $metadata = null): Reserve | bool;
 }
